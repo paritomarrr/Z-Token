ec93ff0e0a99acb96104ee1602079b66e0f8d20e
Pari Tomar <68550407+paritomarrr@users.noreply.github.com>
2022-01-20 16:19:48 +0530

    erc token creation

--- contracts/ZToken.sol
+++ contracts/ZToken.sol
@@ -1,6 +1,12 @@
 pragma solidity >=0.4.2;
 
 contract ZToken {
+    // name
+    string public name = "Z Token";
+    // symbol
+    string public symbol = "ZTC";
+    //standard
+    string public standard = "Z Token v1.0";
     // constructor
     // set the total number of tokens
     // read the total number of token
--- test/ZToken.js
+++ test/ZToken.js
@@ -1,7 +1,30 @@
+// async function getAccount(id) {
+//   let accounts = await web3.eth.getAccounts();
+//   return accounts[id];
+// }
+
 var ZToken = artifacts.require("./ZToken.sol");
 contract("ZToken", function (accounts) {
   var tokenInstance;
-  it("sets the total supply upon deployment", function () {
+  it("initializes the contract with the correct values", function () {
+    return ZToken.deployed()
+      .then(function (instance) {
+        tokenInstance = instance;
+        return tokenInstance.name();
+      })
+      .then(function (name) {
+        assert.equal(name, "Z Token", "has the corect name");
+        return tokenInstance.symbol();
+      })
+      .then(function (symbol) {
+        assert.equal(symbol, "ZTC", "has the correct symbol");
+        return tokenInstance.standard();
+      })
+      .then(function (standard) {
+        assert.equal(standard, "Z Token v1.0", "has the correct standard");
+      });
+  });
+  it("allocates the initial supply upon deployment", function () {
     return ZToken.deployed()
       .then(function (instance) {
         tokenInstance = instance;
